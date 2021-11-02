de78e36479ab8eff40adf9710e9060b088230f36
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-11-02 17:17:51 -0300

    Obtenção de User por ID e tratativa de erros do User

--- src/main/java/principal/services/UserService.java
+++ src/main/java/principal/services/UserService.java
@@ -1,12 +1,14 @@
 package principal.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import principal.domain.User;
 import principal.repository.UserRepository;
+import principal.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Resource not found. Id: " + id));
 	}
 
 }
---
+++ src/main/java/principal/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package principal.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
