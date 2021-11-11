84f3167f34a7e391eae160d359d8ec3d194630ae
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-11-11 08:07:42 -0300

    Criação do método Post do User

--- src/main/java/principal/services/UserService.java
+++ src/main/java/principal/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import principal.domain.User;
+import principal.dto.UserDTO;
 import principal.repository.UserRepository;
 import principal.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Resource not found. Id: " + id));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
 }
