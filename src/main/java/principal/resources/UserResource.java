d783208a1d29abf175a1c8850cc87618e42be9ba
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-11-11 08:27:35 -0300

    Método Update do User

--- src/main/java/principal/resources/UserResource.java
+++ src/main/java/principal/resources/UserResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -55,7 +56,13 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 	
-
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO objDto){
+		User user = service.fromDTO(objDto);
+		user.setId(id);
+		user = service.updateUser(user);
+		return ResponseEntity.noContent().build();
+	}
 
 	
 }
