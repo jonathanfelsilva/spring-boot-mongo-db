d4e68921b3b0e5bef3bb37c20f1b60830973eb3d
Jonathan Silva <jonathanfelsilva@gmail.com>
2021-11-12 08:50:01 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/principal/resources/UserResource.java
+++ src/main/java/principal/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import principal.domain.Post;
 import principal.domain.User;
 import principal.dto.UserDTO;
 import principal.services.UserService;
@@ -64,5 +65,11 @@ public class UserResource {
 		return ResponseEntity.noContent().build();
 	}
 
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> returnPosts(@PathVariable String id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());	
+	}
 	
 }
